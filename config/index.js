285c534cd1ca23507efc0914cfb4f45bc879df77
Wzyan <3101885298@qq.com>
2022-09-09 20:33:31 +0800

    feat: 添加自己的openid

--- config/index.js
+++ config/index.js
@@ -56,9 +56,9 @@ export const config = {
   CALLBACK_USERS: [
     {
       // 一般都填自己
-      name: "自己",
+      name: "啊远",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "oRyeO6YeZqx1h94fYePaFZQWBt9g",
     }, 
   ],
     
